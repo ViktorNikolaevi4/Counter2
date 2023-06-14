5ed9eea5ba7b116042d1fa2ad77434b2693c6651
viktor <87v87@mail.ru>
2023-06-14 14:00:18 +0300

    Initial Commit

---
+++ Counter2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter2
+//
+//  Created by Виктор Корольков on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
